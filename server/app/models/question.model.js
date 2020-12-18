c303bd8ed57039b364a0881c76468552f012af02
stefanparaschivescu <stefparaschivescu@gmail.com>
2020-12-18 22:11:27 +0100

    added scheme

---
+++ server/app/models/question.model.js
@@ -0,0 +1,22 @@
+const { ObjectID } = require("mongodb");
+
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+      {
+        question1: String,
+        question2: String,
+        question3: String,
+        question4: String
+      },
+      { timestamps: true }
+    );
+  
+    schema.method("toJSON", function() {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+  
+    const Questions = mongoose.model("questions", schema);
+    return Questions;
+  };
