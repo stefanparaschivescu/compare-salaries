e0e94fa5084b9d79f76a5431dd0e1dd769fce7dc
stefanparaschivescu <stefparaschivescu@gmail.com>
2020-12-18 22:11:27 +0100

    added scheme

---
+++ server/app/models/review.model.js
@@ -0,0 +1,22 @@
+const { ObjectID } = require("mongodb");
+
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+      {
+        user_id: ObjectID,
+        review: String,
+        date: Date,
+        mark: Number
+      },
+      { timestamps: true }
+    );
+  
+    schema.method("toJSON", function() {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+  
+    const Review = mongoose.model("reviews", schema);
+    return Review;
+  };
