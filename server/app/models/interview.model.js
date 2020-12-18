31624c75480c0097957e88c75f69dd180d80d0a1
stefanparaschivescu <stefparaschivescu@gmail.com>
2020-12-18 22:11:27 +0100

    added scheme

---
+++ server/app/models/interview.model.js
@@ -0,0 +1,24 @@
+const { ObjectID } = require("mongodb");
+
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+      {
+        user_id: ObjectID,
+        opinion: String,
+        difficulty: String,
+        questions: String,
+        time: Number,
+        date: Date
+      },
+      { timestamps: true }
+    );
+  
+    schema.method("toJSON", function() {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+  
+    const Interview = mongoose.model("interviews", schema);
+    return Interview;
+  };
