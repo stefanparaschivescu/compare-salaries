503b219d59a4284641a4805b2767d37807ae36d5
unknown <stefparaschivescu@gmail.com>
2021-01-14 15:20:15 +0100

    updated users model and controller

--- server/app/routes/user.routes.js
+++ server/app/routes/user.routes.js
@@ -7,19 +7,24 @@ module.exports = app => {
     router.post("/", users.create);
   
     // Retrieve all Users
-    router.get("/", users.findAll);
-  
+    router.get("/", users.findUser);
+
     // Retrieve all Users with specific password
-    router.get("/:password", users.findSpecificPassword);
+    router.get("/location/:location", users.findSpecificLocation);
+
+    // Retrieve all Users with specific password
+    router.get("/name/:firstName/:lastName", users.findSpecificUserByName);
   
+    router.get("/email/:email", users.findSpecificEmail);
+
     // Retrieve a single User with id
-    router.get("/:id", users.findOne);
+    router.get("/id/:id", users.findOne);
   
     // Update a User with id
-    router.put("/:id", users.update);
+    router.put("/id/:id", users.update);
   
     // Delete a User with id
-    router.delete("/:id", users.delete);
+    router.delete("/id/:id", users.delete);
   
     // Create a new User
     router.delete("/", users.deleteAll);
